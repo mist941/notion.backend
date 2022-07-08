3b0776d90cee6fc7c0e1fd99c413ae1bc1c04c72
ivanstatkevich <statkevich.ivan999@gmail.com>
2022-07-08 21:34:02 +0300

    added Note

--- Notion.Domain/Note.cs
+++ Notion.Domain/Note.cs
@@ -1,5 +1,11 @@
 ﻿namespace Notion.Domain;
 
-public class Class1
+public class Note
 {
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public string title { get; set; }
+    public string description { get; set; }
+    public DateTime CreateAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
 }
