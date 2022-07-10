64534557c958ef468ea72657edc4176f60f4e1fe
ivanstatkevich <statkevich.ivan999@gmail.com>
2022-07-10 15:21:09 +0300

    added mapping

---
+++ Notion.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,9 @@
+using AutoMapper;
+
+namespace Notes.Application.Common.Mappings;
+
+public interface IMapWith<T>
+{
+    void Mapping(Profile profile) =>
+        profile.CreateMap(typeof(T), GetType());
+}
